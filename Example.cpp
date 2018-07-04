acc054d11a428a6856c41afc895f9a9d52b1b743
Joey <40680599+JoeyW243@users.noreply.github.com>
2018-07-03 18:32:11 -0700

    Update and rename deckOfCardsClass.cpp to Example.cpp

--- Example.cpp
+++ Example.cpp
@@ -1,4 +1,4 @@
-// deckOfCardsClass.cpp : Defines the entry point for the console application.
+// Example program used to show off/test what the class can do
 //
 
 #include <iostream>
@@ -12,30 +12,30 @@ string suitToString(suitType suit); // Changes the suit type to a string
 
 int main()
 {
-	const int HANDSIZE = 10;
+	const int HANDSIZE = 10; //Get a hand of 10
 
-	deckType currentDeck;
-	CARD hand[HANDSIZE];
-	string temp;
+	deckType currentDeck;  //Initialize object
+	CARD hand[HANDSIZE];   //Array of cards with a size of the hand
+	string temp;	       //Temp string used later for cout-ing the suit as a string
 
-	for (int i = 0; i < HANDSIZE; i++) {
+	for (int i = 0; i < HANDSIZE; i++) { //Fill hand with cards
 		hand[i] = currentDeck.getCard();
 	}
 
-	for (int i = 0; i < HANDSIZE; i++) {
+	for (int i = 0; i < HANDSIZE; i++) { //Display all cards
 		temp = suitToString(hand[i].suit);
 		cout << "Card number " << i << " is " << hand[i].num << " of " << temp << endl;
 	}
 
-	cout << "Now shuffling \n...\n...\n...\n";
+	cout << "Now shuffling \n...\n...\n...\n"; //Shuffle cards
 	currentDeck.shuffle();
 
 
-	for (int i = 0; i < HANDSIZE; i++) {
+	for (int i = 0; i < HANDSIZE; i++) { //Fill hand with cards again
 		hand[i] = currentDeck.getCard();
 	}
 
-	for (int i = 0; i < HANDSIZE; i++) {
+	for (int i = 0; i < HANDSIZE; i++) { //Display all cards
 		temp = suitToString(hand[i].suit);
 		cout << "Card number " << i << " is " << hand[i].num << " of " << temp << endl;
 	}
