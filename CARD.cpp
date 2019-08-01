04c9fd9c74a3be525195723a0eee5571ee8690ca
Joey <40680599+JoeyW243@users.noreply.github.com>
2019-07-31 22:58:30 -0700

    Update CARD.cpp

--- CARD.cpp
+++ CARD.cpp
@@ -5,25 +5,41 @@
 using namespace std;
 
 
-CARD *sortHand(CARD *unsortedHand, int size) {
-	CARD hand = *unsortedHand;
-
-}
-
 Hand::Hand()
 {
-	first.card.id = 0;
-	first.next = NULL;
-	first.prev = NULL;
+	first->card.id = 0;
+	first->next = NULL;
+	first->prev = NULL;
 }
 
 Hand::Hand(CARD firstCard) {
-	first.card = firstCard;
-	first.next = NULL;
-	first.prev = NULL;
+	first->card = firstCard;
+	first->next = NULL;
+	first->prev = NULL;
 }
 
 Hand::Hand(CardNode firstCard)
 {
-	first = firstCard;
+	*first = firstCard;
+}
+
+Hand::~Hand() //Delete the linked list
+{
+
+}
+
+void Hand::sortHand()
+{
+
+}
+
+void Hand::addCard(CARD newCard)
+{
+	CardNode *newCardNode = new CardNode;
+	newCardNode->card = newCard;
+	newCardNode->prev = NULL;
+	newCardNode->next = first;
+
+	first->prev = newCardNode;
+
 }
